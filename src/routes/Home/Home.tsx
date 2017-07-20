717742970433676dd70f03fd6481135899588ab0
akatsuki <akatsuki@akamacixo.local>
2017-07-20 18:20:25 +0300

    homepage shit

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
-import { RootState } from "../../store/index";
-import { push } from "react-router-redux";
-import { connect } from "../../tools/react-redux-typescript";
+import {RootState} from "../../store/index";
+import {push} from "react-router-redux";
+import {connect} from "../../tools/react-redux-typescript";
 
-import { RouteComponentProps } from "react-router";
-import { HomeActions } from "../../store/Home.store";
+import {RouteComponentProps} from "react-router";
+import {HomeActions} from "../../store/Home.store";
 
 import styles from "./Home.scss";
 
 import header from "./header-bg.png";
-import { Button } from "react-bootstrap";
+
 
 interface HomeProps {
 
@@ -26,12 +26,38 @@ const mapDispatchToProps = ({
 
 export const Home = connect(mapStateToProps, mapDispatchToProps)(p => (
   <div>
-     <section className={styles.hero}>Testing something</section>
-     <Button>dfdfd</Button>
-     <section>
-       <div className={styles.aller}>
-         We help you to get the ownership on the data that companies have collected about you.
-       </div>
-     </section>
+    <section className={styles.hero}>
+      <div className={styles.QuestionsAboutData}>Questions about Data Protection?</div>
+    </section>
+
+    <section>
+      <div className={styles.aller}>
+
+        <div className={styles.sessionbase1}>
+          <div className={styles.herotextright}>
+            We help you to get the ownership on the data that companies have collected about you.
+          </div>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <div className={styles.flowpic}>
+          </div>
+
+        </div>
+      </div>
+    </section>
+    <section>
+      <div className={styles.aller}>
+
+        <div className={styles.sessionbase2}>
+          <div className={styles.herotextleft}>
+            We help the companies to make it easy to obey the data protection
+            regulation (GDPR).
+          </div>
+          <br/><br/><br/><br/><br/><br/><br/>
+          <div className={styles.flowpic2}>
+          </div>
+
+        </div>
+      </div>
+    </section>
   </div>
 ));
