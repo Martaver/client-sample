e7f3d4cc13306f685c2f2bcbef214656075bc2c4
Sebastian Nemeth <sebastian@ilixir.fi>
2017-07-20 10:45:38 +0300

    Update Home to use scss.

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,23 +1,15 @@
-// import { palette } from "../../styles";
 import * as React from "react";
 import { RootState } from "../../store/index";
 import { push } from "react-router-redux";
 import { MapStateToPropsParam, MapDispatchToPropsFunction, MapDispatchToPropsParam, MapDispatchToPropsObject, ComponentDecorator } from "react-redux";
 import { connect } from "../../tools/react-redux-typescript";
 
-// import { style } from "typestyle";
 import { RouteComponentProps } from "react-router";
 import { HomeActions } from "../../store/Home.store";
 
 import { Toolbar, FlatButton } from "material-ui";
-// import { px, linearGradient, rgba } from "csx/lib";
+import styles from "./Home.scss";
 
-// const styles = {
-//   overlay: style({
-//     height: px(600),
-//     backgroundImage: linearGradient('to bottom', rgba(0, 111, 128, 0.6), "#00ddff")
-//   })
-// };
 
 interface HomeProps {
 
@@ -34,7 +26,7 @@ const mapDispatchToProps = ({
 
 export const Home = connect(mapStateToProps, mapDispatchToProps)(p => (
   <div>
-    {/* <section className={styles.overlay}></section> */}
+     <section className={styles.overlay}>Testing</section>
     <section>We help</section>
   </div>
 ));
